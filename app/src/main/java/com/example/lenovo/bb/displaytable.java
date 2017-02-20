9e44ca9174004bbfdf00932a32441e3deca436f0
Chirag-sam <chiraggsam@gmail.com>
2017-02-20 13:44:12 +0530

    drawable

--- app/src/main/java/com/example/lenovo/bb/displaytable.java
+++ app/src/main/java/com/example/lenovo/bb/displaytable.java
@@ -18,39 +18,59 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class displaytable extends AppCompatActivity  implements View.OnTouchListener{
 
-    String a="Mr.J. Jagadeesan";
-    String b="Mr.R. Augustin Issac";
-    String c="Mr.S. Jayakumar";
-    String d="Mrs.T. Rathika";
-    String e="Mr.R.J. Poovaraghan";
-    String f="Mr. Balika.J.Chelliah";
-    String g="Mrs. Sabitha";
-    String h="Mrs.V. Praveena";
-    String i="Mr.S. Pandiaraj";
-    String j="Mr.S. Arunkumar";
-    String k="Mr.T.H. Feiroz Khan";
-    String l="Mr.B. Dojohn Loyd";
-    String m="Ms.M.S. Bennet Praba";
-    String n="Mrs. Mohanavalli Kirthika";
-    String o="Mrs. Sarada";
-    String p="Mr.S.P. MANIRAJ";
-    String q="Ms.J. Ahalya Mary";
-    String r="Mr. Narendran";
-    String s="Ms. Mrudula Oruganti";
-    String t="Mrs. Kalpana";
-    String u="Mrs. Elavarsi";
-    String v="Mr. Sarooraj";
-    String w="Mr. Muneeswaran";
-    String x="Ms. Akshaya";
-    String y="Mr. Senthil";
-    String z="Ms. Anlet";
-    String a1="Mrs.S. Rajathi";
-    String a2="Ms. Caroline El Fiorenza";
-    String a3="Ms. Deva Hema";
-    String a4="Ms. Angeline.R";
-    String a5="Ms.M.S. Antony Vigil";
+    int staffdrawable[]={
+
+            R.drawable.august,
+            R.drawable.bali,
+            R.drawable.jk,
+            R.drawable.poova,
+            R.drawable.bennet,
+            R.drawable.pandi,
+            R.drawable.praveena,
+            R.drawable.arunkumar,
+            R.drawable.antony,
+            R.drawable.feiroz,
+            R.drawable.dojo,
+            R.drawable.ahalya,
+            R.drawable.mrudu,
+            R.drawable.maniraj,
+            R.drawable.sellam,
+            R.drawable.saroo,
+            R.drawable.caroline,
+            R.drawable.angelin,
+            R.drawable.naren,
+            R.drawable.sabitha,
+            R.drawable.sarada,
+            R.drawable.kalpu,
+            R.drawable.senthil,
+            R.drawable.bhaskar,
+            R.drawable.prabu,
+            R.drawable.shiny,
+            R.drawable.ksubha,
+            R.drawable.adlene,
+            R.drawable.sharmila,
+            R.drawable.vishnu,
+            R.drawable.sathya,
+            R.drawable.rajeswari,
+            R.drawable.usha,
+            R.drawable.saraswathi,
+            R.drawable.saravanan,
+            R.drawable.ganesan,
+            R.drawable.sathish,
+            R.drawable.divagar,
+            R.drawable.nandhini,
+            R.drawable.shivanandhini,
+            R.drawable.kalaiarasi,
+            R.drawable.shanthalakshmi,
+            R.drawable.subha
+
+    };
 
 
 
@@ -77,69 +97,11 @@ public class displaytable extends AppCompatActivity  implements View.OnTouchList
             setContentView(R.layout.activity_displaytable);
             ImageView view=(ImageView)findViewById(R.id.view);
             String recv=getIntent().getStringExtra("mytext");
-            if(recv.equals(a))
-                view.setImageResource(R.drawable.hod);
-            else if(recv.equals(b))
-                view.setImageResource(R.drawable.august);
-            else if(recv.equals(c))
-                view.setImageResource(R.drawable.jk);
-            else if(recv.equals(d))
-                view.setImageResource(R.drawable.rathika);
-            else if(recv.equals(e))
-                view.setImageResource(R.drawable.poova);
-            else if(recv.equals(f))
-                view.setImageResource(R.drawable.bali);
-            else if(recv.equals(g))
-                view.setImageResource(R.drawable.sabitha);
-            else if(recv.equals(h))
-                view.setImageResource(R.drawable.praveena);
-            else if(recv.equals(i))
-                view.setImageResource(R.drawable.pandi);
-            else if(recv.equals(j))
-                view.setImageResource(R.drawable.arunkumar);
-            else if(recv.equals(k))
-                view.setImageResource(R.drawable.feiroz);
-            else if(recv.equals(l))
-                view.setImageResource(R.drawable.dojo);
-            else if(recv.equals(m))
-                view.setImageResource(R.drawable.bennet);
-            else if(recv.equals(n))
-                view.setImageResource(R.drawable.mohanavalli);
-            else if(recv.equals(o))
-                view.setImageResource(R.drawable.sarada);
-            else if(recv.equals(p))
-                view.setImageResource(R.drawable.maniraj);
-            else if(recv.equals(q))
-                view.setImageResource(R.drawable.ahalya);
-            else if(recv.equals(r))
-                view.setImageResource(R.drawable.naren);
-            else if(recv.equals(s))
-                view.setImageResource(R.drawable.mrudu);
-            else if(recv.equals(t))
-                view.setImageResource(R.drawable.kalpu);
-            else if(recv.equals(u))
-                view.setImageResource(R.drawable.elaa);
-            else if(recv.equals(v))
-                view.setImageResource(R.drawable.saroo);
-            else if(recv.equals(w))
-                view.setImageResource(R.drawable.munee);
-            else if(recv.equals(x))
-                view.setImageResource(R.drawable.akshaya);
-            else if(recv.equals(y))
-                view.setImageResource(R.drawable.senthil);
-            else if(recv.equals(z))
-                view.setImageResource(R.drawable.anlet);
-            else if(recv.equals(a1))
-                view.setImageResource(R.drawable.rajathi);
-            else if(recv.equals(a2))
-                view.setImageResource(R.drawable.caroline);
-            else if(recv.equals(a3))
-                view.setImageResource(R.drawable.deva);
-            else if(recv.equals(a4))
-                view.setImageResource(R.drawable.angelin);
-            else if(recv.equals(a5))
-                view.setImageResource(R.drawable.antony);
-            else
+            String x[]=getResources().getStringArray(R.array.Staff_Name);
+            ArrayList<String> lis=new ArrayList<>(Arrays.asList(x));
+            int pos=lis.indexOf(recv);
+            view.setImageResource(staffdrawable[pos]);
+            if (pos==-1)
                 view.setImageResource(R.mipmap.b_board);
 
             view.setOnTouchListener(this);
