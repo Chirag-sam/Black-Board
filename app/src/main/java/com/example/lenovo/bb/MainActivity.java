7553910ba01a4d72803d557e76218896efeea0a8
vijay krishna <krsnvijay@gmail.com>
2017-02-21 19:33:01 +0530

    stuff

--- app/src/main/java/com/example/lenovo/bb/MainActivity.java
+++ app/src/main/java/com/example/lenovo/bb/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.vstechlab.easyfonts.EasyFonts;
@@ -15,9 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView)findViewById(R.id.tv);
 
-        tv.setTypeface(EasyFonts.captureIt(this));
+
         Button button = (Button)findViewById(R.id.button);
         Button button2 = (Button)findViewById(R.id.button2);
         Button button3 = (Button)findViewById(R.id.button3);
