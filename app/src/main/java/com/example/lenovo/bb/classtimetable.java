75f99837654a71c96f94c6dc28b5a11f2b32a028
Chirag-sam <chiraggsam@gmail.com>
2017-02-21 11:50:11 +0530

    class time table

--- app/src/main/java/com/example/lenovo/bb/classtimetable.java
+++ app/src/main/java/com/example/lenovo/bb/classtimetable.java
@@ -1,5 +1,6 @@
 package com.example.lenovo.bb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,36 +12,87 @@ import android.widget.Toast;
 
 public class classtimetable extends AppCompatActivity {
 
-    String[] dept = {"CSE","EEE","Mech","IT"};
+
     String[] year = {"1","2","3","4"};
-    String[] sect = {"A","B","C","D","E","F","G","H","I","J","K","L"};
+    String[] sect1 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
+    String[] sect2 = {"A","B","C","D","E","F","G","H","I","J","K"};
+    String[] sect3 = {"A","B","C","D","E"};
+    String[] sect4 = {"A"};
+
+    int year1[]={
+            R.drawable.a1,
+            R.drawable.b1,
+            R.drawable.c1,
+            R.drawable.d1,
+            R.drawable.e1,
+            R.drawable.f1,
+            R.drawable.g1,
+            R.drawable.h1,
+            R.drawable.i1,
+            R.drawable.j1,
+            R.drawable.k1,
+            R.drawable.l1,
+            R.drawable.m1,
+            R.drawable.n1,
+            R.drawable.o1
+    };
+    int year2[]={
+            R.drawable.a2,
+            R.drawable.b2,
+            R.drawable.c2,
+            R.drawable.d2,
+            R.drawable.e2,
+            R.drawable.f2,
+            R.drawable.g2,
+            R.drawable.h2,
+            R.drawable.i2,
+            R.drawable.j2,
+            R.drawable.k2
+    };
+    int year3[]={
+            R.drawable.a3,
+            R.drawable.b3,
+            R.drawable.c3,
+            R.drawable.d3,
+            R.drawable.e3
+    };
+    int year4[]={
+            R.drawable.rr4
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classtimetable);
-        final AutoCompleteTextView at1 = (AutoCompleteTextView)findViewById(R.id.at1);
+
         final AutoCompleteTextView at2 = (AutoCompleteTextView)findViewById(R.id.at2);
         final AutoCompleteTextView at3 = (AutoCompleteTextView)findViewById(R.id.at3);
         Button button = (Button)findViewById(R.id.button);
-        ImageView down1 = (ImageView)findViewById(R.id.down1);
+
         ImageView down2 = (ImageView)findViewById(R.id.down2);
         ImageView down3 = (ImageView)findViewById(R.id.down3);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,dept);
+
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,year);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,sect);
-        at1.setAdapter(adapter);
+        final ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,sect1);
+        final ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,sect2);
+        final ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,sect3);
+        final ArrayAdapter<String> adapter5 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,sect4);
+
         at2.setAdapter(adapter1);
-        at3.setAdapter(adapter2);
-        at1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                at1.showDropDown();
-            }
-        });
+
+
         at2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 at2.showDropDown();
+                String str2 = at2.getText().toString();
+                if(str2.equals("1"))
+                    at2.setAdapter(adapter2);
+                else if(str2.equals("2"))
+                    at2.setAdapter(adapter3);
+                else if(str2.equals("3"))
+                    at2.setAdapter(adapter4);
+                else if(str2.equals("4"))
+                    at2.setAdapter(adapter5);
             }
         });
         at3.setOnClickListener(new View.OnClickListener() {
@@ -49,12 +101,7 @@ public class classtimetable extends AppCompatActivity {
                 at3.showDropDown();
             }
         });
-        down1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                at1.showDropDown();
-            }
-        });
+
         down2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,9 +117,29 @@ public class classtimetable extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String str = at1.getText().toString();
+
                 String str2 = at2.getText().toString();
                 String str3 = at3.getText().toString();
+                if (str2.equals("1"))
+                { Intent intent = new Intent(getBaseContext(), displayclasstable.class);
+                intent.putExtra("drawable", year1[Integer.parseInt(str3)]);
+                startActivity(intent);}
+                else if (str2.equals("2"))
+                { Intent intent = new Intent(getBaseContext(), displayclasstable.class);
+                    intent.putExtra("drawable", year2[Integer.parseInt(str3)]);
+                    startActivity(intent);}
+                else if (str2.equals("3"))
+                { Intent intent = new Intent(getBaseContext(), displayclasstable.class);
+                    intent.putExtra("drawable", year3[Integer.parseInt(str3)]);
+                    startActivity(intent);}
+                else if (str2.equals("4"))
+                { Intent intent = new Intent(getBaseContext(), displayclasstable.class);
+                    intent.putExtra("drawable", year4[Integer.parseInt(str3)]);
+                    startActivity(intent);}
+
+
+
+
 
             }
         });
---
+++ new_db.db.sql
@@ -0,0 +1,267 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Wednesday" (
+	`Name`	TEXT,
+	`p1`	INTEGER,
+	`p2`	INTEGER,
+	`p3`	INTEGER,
+	`p4`	INTEGER,
+	`p5`	INTEGER,
+	`p6`	INTEGER,
+	`p7`	INTEGER
+);
+INSERT INTO `Wednesday` VALUES ('Mr.R. Augustian Isaac',0,0,1,1,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr. Balika.J.Chelliah',1,1,0,1,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr.S. Jaya Kumar',0,1,0,0,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr.R.J. Poovaraghan',1,1,1,0,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Ms.M.S. Bennet Praba',0,1,1,1,0,1,0);
+INSERT INTO `Wednesday` VALUES ('Mr.S. Pandiaraj',1,1,0,0,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Ms.V. Praveena',0,0,0,1,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr.S. Arun Kumar',1,1,0,0,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.M.S. Antony Vigil',0,1,0,0,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Mr.T.H. Feiroz Khan',0,0,0,1,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Mr. B. Dojohn Loyd',0,0,1,0,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs. J. Ahalya Mary',0,0,1,1,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Mrs. Mrudula Oruganti',1,1,0,1,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mr. S.P. Maniraj',1,0,0,0,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Ms. V. Sellam',0,0,0,1,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr. Sarooraj',0,0,0,1,0,1,0);
+INSERT INTO `Wednesday` VALUES ('Ms. Caroline El Fiorenza',1,1,0,1,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs. R. Angeline',1,0,0,0,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mr. Narendran M',0,0,0,1,1,1,1);
+INSERT INTO `Wednesday` VALUES ('Mrs. Sabitha P',1,0,1,0,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs. Tadepalli  Sarada Kiranmayee',0,0,0,0,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.G. Kalpana',1,1,0,0,0,1,1);
+INSERT INTO `Wednesday` VALUES ('Mr. Senthil M',1,0,1,1,0,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr.D. Bhaskar',0,0,0,0,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mr.M. Prabu',1,0,0,0,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.A. Shiny',0,0,1,1,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Ms.K. Suba',1,0,1,1,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Ms.P. Adlene Ebenezer',0,1,0,1,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.A. Sharmila Agnal',1,1,0,0,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.S. Vishu Dharsini',1,0,0,0,1,0,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.V. Sathya',1,0,0,0,0,1,0);
+INSERT INTO `Wednesday` VALUES ('Ms.S.R. Rajeswari',0,1,0,0,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Ms.S. Usha Krithika',0,0,0,0,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mrs.E. Saraswathi',0,1,1,0,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Mr.M. Saravanan',1,0,1,1,0,1,0);
+INSERT INTO `Wednesday` VALUES ('Mr.D. Ganesan',0,0,1,1,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Mr.K. Sathish',0,0,1,0,0,0,0);
+INSERT INTO `Wednesday` VALUES ('Mr. Divagar Balasubramanian',1,0,0,0,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Ms.S. Nandhini',0,0,1,1,0,1,0);
+INSERT INTO `Wednesday` VALUES ('Ms.J. Shiva Nandhini',0,0,0,0,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Mrs.S. Kalaiarasi',0,0,0,0,1,1,0);
+INSERT INTO `Wednesday` VALUES ('Mrs.M. Shanthalakshmi',0,1,0,0,1,0,1);
+INSERT INTO `Wednesday` VALUES ('Ms.P. Subashini',0,0,1,0,1,0,0);
+CREATE TABLE "Tuesday" (
+	`Name`	TEXT,
+	`p1`	INTEGER,
+	`p2`	INTEGER,
+	`p3`	INTEGER,
+	`p4`	INTEGER,
+	`p5`	INTEGER,
+	`p6`	INTEGER,
+	`p7`	INTEGER
+);
+INSERT INTO `Tuesday` VALUES ('Mr.R. Augustian Isaac',1,1,0,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr. Balika.J.Chelliah',0,1,1,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr.S. Jaya Kumar',1,0,0,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr.R.J. Poovaraghan',0,0,0,1,0,0,0);
+INSERT INTO `Tuesday` VALUES ('Ms.M.S. Bennet Praba',1,1,0,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr.S. Pandiaraj',0,0,0,0,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Ms.V. Praveena',1,0,0,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr.S. Arun Kumar',0,0,1,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.M.S. Antony Vigil',1,1,0,0,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Mr.T.H. Feiroz Khan',0,0,1,1,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mr. B. Dojohn Loyd',0,0,0,0,0,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs. J. Ahalya Mary',1,0,0,0,0,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs. Mrudula Oruganti',0,0,0,1,1,1,0);
+INSERT INTO `Tuesday` VALUES ('Mr. S.P. Maniraj',0,1,0,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Ms. V. Sellam',0,0,0,1,1,1,0);
+INSERT INTO `Tuesday` VALUES ('Mr. Sarooraj',0,0,0,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Ms. Caroline El Fiorenza',1,1,0,1,0,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs. R. Angeline',0,0,1,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr. Narendran M',0,1,0,0,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mrs. Sabitha P',1,1,0,0,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mrs. Tadepalli  Sarada Kiranmayee',0,0,1,0,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Mrs.G. Kalpana',0,0,0,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr. Senthil M',1,1,0,0,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mr.D. Bhaskar',0,0,0,0,1,1,0);
+INSERT INTO `Tuesday` VALUES ('Mr.M. Prabu',0,1,0,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.A. Shiny',1,1,0,1,0,0,0);
+INSERT INTO `Tuesday` VALUES ('Ms.K. Suba',0,1,0,0,1,1,0);
+INSERT INTO `Tuesday` VALUES ('Ms.P. Adlene Ebenezer',1,0,0,0,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.A. Sharmila Agnal',1,0,1,1,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Mrs.S. Vishu Dharsini',0,0,1,0,1,1,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.V. Sathya',1,1,1,0,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Ms.S.R. Rajeswari',0,0,0,0,1,1,0);
+INSERT INTO `Tuesday` VALUES ('Ms.S. Usha Krithika',0,0,1,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.E. Saraswathi',0,0,0,0,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mr.M. Saravanan',1,0,0,0,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Mr.D. Ganesan',1,0,0,0,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Mr.K. Sathish',0,0,0,0,1,0,1);
+INSERT INTO `Tuesday` VALUES ('Mr. Divagar Balasubramanian',0,0,0,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Ms.S. Nandhini',0,0,0,1,1,0,0);
+INSERT INTO `Tuesday` VALUES ('Ms.J. Shiva Nandhini',0,0,0,1,0,0,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.S. Kalaiarasi',1,0,1,1,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Mrs.M. Shanthalakshmi',0,0,0,0,0,1,0);
+INSERT INTO `Tuesday` VALUES ('Ms.P. Subashini',1,0,0,0,1,1,0);
+CREATE TABLE "Thursday" (
+	`Name`	TEXT,
+	`p1`	INTEGER,
+	`p2`	INTEGER,
+	`p3`	INTEGER,
+	`p4`	INTEGER,
+	`p5`	INTEGER,
+	`p6`	INTEGER,
+	`p7`	INTEGER
+);
+INSERT INTO `Thursday` VALUES ('Mr.R. Augustian Isaac',0,0,0,0,0,0,1);
+INSERT INTO `Thursday` VALUES ('Mr. Balika.J.Chelliah',0,0,0,0,1,1,0);
+INSERT INTO `Thursday` VALUES ('Mr.S. Jaya Kumar',0,0,1,0,0,1,1);
+INSERT INTO `Thursday` VALUES ('Mr.R.J. Poovaraghan',1,0,0,0,1,0,1);
+INSERT INTO `Thursday` VALUES ('Ms.M.S. Bennet Praba',0,0,1,0,1,0,1);
+INSERT INTO `Thursday` VALUES ('Mr.S. Pandiaraj',1,0,0,0,0,0,0);
+INSERT INTO `Thursday` VALUES ('Ms.V. Praveena',1,1,0,0,1,0,1);
+INSERT INTO `Thursday` VALUES ('Mr.S. Arun Kumar',1,1,0,1,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs.M.S. Antony Vigil',0,0,0,1,0,0,0);
+INSERT INTO `Thursday` VALUES ('Mr.T.H. Feiroz Khan',1,0,1,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mr. B. Dojohn Loyd',1,1,1,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs. J. Ahalya Mary',1,0,0,0,0,1,0);
+INSERT INTO `Thursday` VALUES ('Mrs. Mrudula Oruganti',0,0,0,1,1,1,0);
+INSERT INTO `Thursday` VALUES ('Mr. S.P. Maniraj',1,1,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Ms. V. Sellam',0,0,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mr. Sarooraj',0,0,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Ms. Caroline El Fiorenza',0,0,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs. R. Angeline',0,1,0,0,1,0,1);
+INSERT INTO `Thursday` VALUES ('Mr. Narendran M',0,0,0,0,0,1,1);
+INSERT INTO `Thursday` VALUES ('Mrs. Sabitha P',0,0,0,0,1,1,0);
+INSERT INTO `Thursday` VALUES ('Mrs. Tadepalli  Sarada Kiranmayee',1,0,0,0,1,0,1);
+INSERT INTO `Thursday` VALUES ('Mrs.G. Kalpana',0,0,0,1,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mr. Senthil M',0,0,0,0,0,1,0);
+INSERT INTO `Thursday` VALUES ('Mr.D. Bhaskar',0,0,1,1,1,1,0);
+INSERT INTO `Thursday` VALUES ('Mr.M. Prabu',1,0,0,0,0,1,0);
+INSERT INTO `Thursday` VALUES ('Mrs.A. Shiny',0,0,1,0,0,0,0);
+INSERT INTO `Thursday` VALUES ('Ms.K. Suba',0,0,1,0,0,1,0);
+INSERT INTO `Thursday` VALUES ('Ms.P. Adlene Ebenezer',1,1,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs.A. Sharmila Agnal',0,0,0,1,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs.S. Vishu Dharsini',1,0,0,0,0,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs.V. Sathya',0,0,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Ms.S.R. Rajeswari',1,0,0,0,0,1,1);
+INSERT INTO `Thursday` VALUES ('Ms.S. Usha Krithika',1,0,0,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs.E. Saraswathi',0,0,1,0,1,1,0);
+INSERT INTO `Thursday` VALUES ('Mr.M. Saravanan',0,0,0,0,0,1,0);
+INSERT INTO `Thursday` VALUES ('Mr.D. Ganesan',0,0,0,1,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mr.K. Sathish',0,1,0,0,0,0,0);
+INSERT INTO `Thursday` VALUES ('Mr. Divagar Balasubramanian',1,0,0,0,0,0,0);
+INSERT INTO `Thursday` VALUES ('Ms.S. Nandhini',0,0,0,0,0,0,0);
+INSERT INTO `Thursday` VALUES ('Ms.J. Shiva Nandhini',1,1,0,1,1,0,1);
+INSERT INTO `Thursday` VALUES ('Mrs.S. Kalaiarasi',1,0,1,0,1,0,0);
+INSERT INTO `Thursday` VALUES ('Mrs.M. Shanthalakshmi',0,0,0,1,1,0,1);
+INSERT INTO `Thursday` VALUES ('Ms.P. Subashini',0,0,0,1,0,1,0);
+CREATE TABLE "Monday" (
+	`Name`	TEXT,
+	`p1`	INTEGER,
+	`p2`	INTEGER,
+	`p3`	INTEGER,
+	`p4`	INTEGER,
+	`p5`	INTEGER,
+	`p6`	INTEGER,
+	`p7`	INTEGER
+);
+INSERT INTO `Monday` VALUES ('Mr.R. Augustian Isaac',1,0,1,1,1,0,0);
+INSERT INTO `Monday` VALUES ('Mr. Balika.J.Chelliah',1,1,0,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Mr.S. Jaya Kumar',1,0,0,1,1,0,0);
+INSERT INTO `Monday` VALUES ('Mr.R.J. Poovaraghan',0,0,1,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Ms.M.S. Bennet Praba',0,0,0,0,1,0,0);
+INSERT INTO `Monday` VALUES ('Mr.S. Pandiaraj',1,0,1,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Ms.V. Praveena',0,0,0,0,0,0,0);
+INSERT INTO `Monday` VALUES ('Mr.S. Arun Kumar',0,0,0,0,0,1,0);
+INSERT INTO `Monday` VALUES ('Mrs.M.S. Antony Vigil',1,0,0,0,1,0,0);
+INSERT INTO `Monday` VALUES ('Mr.T.H. Feiroz Khan',0,0,0,0,0,0,0);
+INSERT INTO `Monday` VALUES ('Mr. B. Dojohn Loyd',1,1,0,0,0,1,0);
+INSERT INTO `Monday` VALUES ('Mrs. J. Ahalya Mary',0,0,1,1,1,0,0);
+INSERT INTO `Monday` VALUES ('Mrs. Mrudula Oruganti',0,0,1,0,0,0,0);
+INSERT INTO `Monday` VALUES ('Mr. S.P. Maniraj',1,1,0,0,0,1,0);
+INSERT INTO `Monday` VALUES ('Ms. V. Sellam',1,1,0,0,0,1,0);
+INSERT INTO `Monday` VALUES ('Mr. Sarooraj',1,1,1,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Ms. Caroline El Fiorenza',0,1,0,1,1,1,0);
+INSERT INTO `Monday` VALUES ('Mrs. R. Angeline',0,0,1,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Mr. Narendran M',0,0,0,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Mrs. Sabitha P',1,0,1,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Mrs. Tadepalli  Sarada Kiranmayee',0,0,0,1,1,0,1);
+INSERT INTO `Monday` VALUES ('Mrs.G. Kalpana',0,0,0,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Mr. Senthil M',0,1,0,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Mr.D. Bhaskar',0,0,0,1,1,0,0);
+INSERT INTO `Monday` VALUES ('Mr.M. Prabu',0,1,1,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Mrs.A. Shiny',1,1,0,0,1,0,0);
+INSERT INTO `Monday` VALUES ('Ms.K. Suba',0,0,0,0,1,0,0);
+INSERT INTO `Monday` VALUES ('Ms.P. Adlene Ebenezer',0,0,0,0,0,0,0);
+INSERT INTO `Monday` VALUES ('Mrs.A. Sharmila Agnal',0,0,0,1,0,0,0);
+INSERT INTO `Monday` VALUES ('Mrs.S. Vishu Dharsini',0,1,0,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Mrs.V. Sathya',0,0,1,0,1,0,0);
+INSERT INTO `Monday` VALUES ('Ms.S.R. Rajeswari',0,0,0,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Ms.S. Usha Krithika',0,0,1,1,1,0,1);
+INSERT INTO `Monday` VALUES ('Mrs.E. Saraswathi',1,0,1,1,0,0,0);
+INSERT INTO `Monday` VALUES ('Mr.M. Saravanan',0,0,1,1,0,0,0);
+INSERT INTO `Monday` VALUES ('Mr.D. Ganesan',0,0,1,1,1,1,0);
+INSERT INTO `Monday` VALUES ('Mr.K. Sathish',1,1,0,1,0,0,0);
+INSERT INTO `Monday` VALUES ('Mr. Divagar Balasubramanian',0,1,0,1,1,1,0);
+INSERT INTO `Monday` VALUES ('Ms.S. Nandhini',1,0,0,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Ms.J. Shiva Nandhini',1,0,0,0,1,1,0);
+INSERT INTO `Monday` VALUES ('Mrs.S. Kalaiarasi',0,1,0,0,1,0,1);
+INSERT INTO `Monday` VALUES ('Mrs.M. Shanthalakshmi',1,0,1,0,0,1,0);
+INSERT INTO `Monday` VALUES ('Ms.P. Subashini',0,0,0,0,1,0,1);
+CREATE TABLE "Friday" (
+	`Name`	TEXT,
+	`p1`	INTEGER,
+	`p2`	INTEGER,
+	`p3`	INTEGER,
+	`p4`	INTEGER,
+	`p5`	INTEGER,
+	`p6`	INTEGER,
+	`p7`	INTEGER
+);
+INSERT INTO `Friday` VALUES ('Mr.R. Augustian Isaac',1,0,1,1,1,0,1);
+INSERT INTO `Friday` VALUES ('Mr. Balika.J.Chelliah',0,0,1,1,0,0,0);
+INSERT INTO `Friday` VALUES ('Mr.S. Jaya Kumar',0,1,0,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr.R.J. Poovaraghan',0,1,0,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Ms.M.S. Bennet Praba',0,0,0,0,1,0,1);
+INSERT INTO `Friday` VALUES ('Mr.S. Pandiaraj',0,0,1,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Ms.V. Praveena',1,0,1,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr.S. Arun Kumar',1,1,0,0,0,1,1);
+INSERT INTO `Friday` VALUES ('Mrs.M.S. Antony Vigil',0,1,0,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr.T.H. Feiroz Khan',0,1,1,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr. B. Dojohn Loyd',1,0,1,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mrs. J. Ahalya Mary',0,0,0,0,0,1,0);
+INSERT INTO `Friday` VALUES ('Mrs. Mrudula Oruganti',0,1,0,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr. S.P. Maniraj',0,0,1,1,0,1,0);
+INSERT INTO `Friday` VALUES ('Ms. V. Sellam',1,0,1,1,0,0,0);
+INSERT INTO `Friday` VALUES ('Mr. Sarooraj',0,0,1,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Ms. Caroline El Fiorenza',0,0,0,0,0,1,0);
+INSERT INTO `Friday` VALUES ('Mrs. R. Angeline',0,0,1,1,0,1,0);
+INSERT INTO `Friday` VALUES ('Mr. Narendran M',1,0,1,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Mrs. Sabitha P',0,0,1,1,0,0,0);
+INSERT INTO `Friday` VALUES ('Mrs. Tadepalli  Sarada Kiranmayee',1,1,0,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Mrs.G. Kalpana',0,0,0,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr. Senthil M',0,0,1,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr.D. Bhaskar',0,0,0,0,1,1,0);
+INSERT INTO `Friday` VALUES ('Mr.M. Prabu',0,0,0,0,0,1,0);
+INSERT INTO `Friday` VALUES ('Mrs.A. Shiny',0,1,0,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Ms.K. Suba',1,1,0,0,1,1,0);
+INSERT INTO `Friday` VALUES ('Ms.P. Adlene Ebenezer',1,0,1,1,1,0,1);
+INSERT INTO `Friday` VALUES ('Mrs.A. Sharmila Agnal',0,0,1,0,1,0,1);
+INSERT INTO `Friday` VALUES ('Mrs.S. Vishu Dharsini',0,0,1,1,1,0,1);
+INSERT INTO `Friday` VALUES ('Mrs.V. Sathya',0,1,0,0,0,0,0);
+INSERT INTO `Friday` VALUES ('Ms.S.R. Rajeswari',0,0,0,1,1,0,1);
+INSERT INTO `Friday` VALUES ('Ms.S. Usha Krithika',0,0,0,0,1,1,0);
+INSERT INTO `Friday` VALUES ('Mrs.E. Saraswathi',1,1,0,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr.M. Saravanan',1,0,1,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr.D. Ganesan',0,1,0,0,1,1,0);
+INSERT INTO `Friday` VALUES ('Mr.K. Sathish',1,1,0,0,1,0,0);
+INSERT INTO `Friday` VALUES ('Mr. Divagar Balasubramanian',0,0,1,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Ms.S. Nandhini',0,1,0,1,1,0,0);
+INSERT INTO `Friday` VALUES ('Ms.J. Shiva Nandhini',0,0,0,0,1,0,1);
+INSERT INTO `Friday` VALUES ('Mrs.S. Kalaiarasi',0,0,1,0,0,1,1);
+INSERT INTO `Friday` VALUES ('Mrs.M. Shanthalakshmi',1,0,1,1,0,0,0);
+INSERT INTO `Friday` VALUES ('Ms.P. Subashini',1,0,1,1,1,0,0);
+COMMIT;
