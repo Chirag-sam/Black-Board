c8bb6b8fc7a568c13b424851055543dbb3387575
Chirag-sam <chiraggsam@gmail.com>
2017-03-07 15:38:53 +0530

    class time table

--- app/src/main/java/com/example/lenovo/bb/MainActivity.java
+++ app/src/main/java/com/example/lenovo/bb/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.vstechlab.easyfonts.EasyFonts;
@@ -16,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         Button button = (Button)findViewById(R.id.button);
         Button button2 = (Button)findViewById(R.id.button2);
